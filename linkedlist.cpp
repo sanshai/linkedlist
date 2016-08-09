8c3d0b3e54a2d4b7ad55ae8e3e851ed64db656d5
sanshai <415988436@qq.com>
2016-08-09 15:14:04 +0800

    初步完成单向链表

---
+++ linkedlist.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <stdio.h>
+
+template<class T>
+class KListNode
+{
+public:
+    KListNode();
+    T            m_Value;
+    KListNode    *m_pNext;
+};
+
+template<class T>
+KListNode<T> ::KListNode()
+{
+    m_pNext = NULL;
+}
+
+template <class T>
+class KList:private KListNode<T>
+{
+public:
+    KList();
+    void            UnInit();
+    T               PopFront();
+    bool            PushBack(T x);
+    bool            IsEmpty();
+    unsigned int    GetSize();
+
+private:
+    unsigned int    m_uListLength;
+    KListNode<T>    *m_LastNode;
+    KListNode<T>    *m_HeadNode;
+    KListNode<T>    *m_Node;///temp node
+};
+
+template <class T>
+KList<T> ::KList()
+{
+    m_Node          = NULL;
+    m_HeadNode      = NULL;
+    m_LastNode      = NULL;
+    m_uListLength   = 0;
+}
+
+template <class T>
+void KList<T> ::UnInit()
+{
+    while (PopFront());
+}
+
+template <class T>
+T KList<T> ::PopFront()
+{
+    T        TempValue;
+    KListNode<T> *TempNode = m_HeadNode;
+    if (TempNode == NULL)
+        return false;
+    else
+    {
+        TempValue  = TempNode->m_Value;
+        m_HeadNode = TempNode->m_pNext;
+        if (!TempNode->m_pNext)
+            m_LastNode = NULL;
+        delete TempNode;
+        m_uListLength--;
+        return TempValue;
+    }
+}
+
+template <class T>
+bool KList<T> ::PushBack(T x)
+{
+    m_Node = new KListNode<T>();
+    if (!m_Node)
+        return false;
+    m_Node->m_Value = x;
+    if (!m_LastNode)
+    {
+        m_HeadNode = m_Node;
+        m_LastNode = m_Node;
+    }
+    else
+    {
+        m_LastNode->m_pNext = m_Node;
+        m_LastNode          = m_Node;
+    }
+    m_uListLength++;
+    return true;
+}
+
+template <class T>
+bool KList<T> ::IsEmpty()
+{
+    return m_uListLength == 0;
+}
+
+template <class T>
+unsigned int KList<T> ::GetSize()
+{
+    return m_uListLength;
+}
+
+int main()
+{
+    KList<int> TempAddr;
+    for (int i = 0; i < 10000000; ++i)
+    {
+        TempAddr.PushBack(123456789);
+    }
+    std::cout << TempAddr.PopFront();
+    TempAddr.UnInit();
+    std::cout << "\n" << TempAddr.GetSize();
+    std::cout << TempAddr.PopFront();
+    char a;
+    std::cin >> a;
+    return 0;
+}
